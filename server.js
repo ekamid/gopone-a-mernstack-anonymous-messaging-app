c179195395b1105487df5fdab675c63a3358cb51
Ebrahim Khalil <ebrahimkhalilamidbd@gmail.com>
2020-04-11 00:50:16 +0600

    Verson 1.0.1

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 //init app
 const app = express();
@@ -10,16 +11,20 @@ connectDB();
 //express parser middleware
 app.use(express.json({ extended: true }));
 
-//for heroku deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 //routes
 app.use("/api/register", require("./routes/register"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/messages", require("./routes/messages"));
 app.use("/api/", require("./routes/message"));
+
+//for heroku deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
